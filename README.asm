e5113284d618c7c7508e7c2bbf47643b7e0d7215
areebatariq01 <areebatariq697@gmail.com>
2024-12-02 12:27:18 +0500

    read-file

---
+++ README.asm
@@ -0,0 +1,31 @@
+# asm-github1
+
+Solve all code file 
+compile and check proper running
+after each program commit with proper comment and push
+give the proper remarks here in readme
+
+
+reg #   ------------  Name --------
+
+
+
+remarks about sum-even
+
+This assembly language program calculates and displays the sum and count of even numbers in an array.
+It iterates through the array, checks for even numbers, and updates the sum and count accordingly.
+
+
+
+remarks about sum-odd
+
+This assembly language program calculates and displays the sum and count of odd numbers in an array.
+
+
+remarks about sum-pos
+
+This assembly language program calculates and displays the sum and count of positive numbers in an array.
+It uses a mask (8000h) to check for negative numbers and skips them, updating the sum and count only for positive numbers
+
+
+Overall remarks
